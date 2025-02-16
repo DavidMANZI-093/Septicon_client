adf9fe4ee15e361f39da8641c1d836e4042c1e8d
David MANZI <manzidavid093@gmail.com>
2025-02-16 17:30:53 +0200

    Auto-commit at 16/02/2025, 17:30:53 for inactivity period...

--- src/components/blank-feature.tsx
+++ src/components/blank-feature.tsx
@@ -8,8 +8,10 @@ import Loader2 from './ui/loaders/loader-2';
 const BlankFeature = (/* props: Props */) => {
     return (
         <div className='relative flex flex-col items-center justify-center w-full h-full gap-4'>
-            <Loader2 />
-            <p className='relative flex text-zinc-600 font-medium text-sm'>Feature is under development...</p>
+            <div className='relative flex w-fit h-fit items-center justify-center border-zinc-800'>
+                <Loader2 />
+                <p className='relative flex text-zinc-600 font-medium text-sm'>Feature is under development...</p>
+            </div>
         </div>
     )
 }
